2f9459632ade88f68fc2b73606834346dc8c730d
Supragya Raj <supragyaraj@gmail.com>
2022-09-10 21:18:13 +0530

    init: base impl

--- masterSlavePool.go
+++ masterSlavePool.go
@@ -1,6 +1,7 @@
 package masterslavepool
 
 import (
+	"errors"
 	"sort"
 	"sync"
 	"time"
@@ -10,10 +11,12 @@ import (
 )
 
 type PoolNodeMeta struct {
-	IsAlive    bool
-	Reports    []time.Time
-	LastReport time.Time
-	BringAlive time.Time
+	IsAlive     bool
+	Reports     uint32
+	reportMutex sync.Mutex
+	FirstReport time.Time
+	LastReport  time.Time
+	BringAlive  time.Time
 }
 
 type PoolNode[I any] struct {
@@ -22,9 +25,13 @@ type PoolNode[I any] struct {
 }
 
 type MasterSlavePool[I any] struct {
-	rwlock sync.RWMutex
-	Master *PoolNode[I]
-	Slaves []*PoolNode[I]
+	rwlock               sync.RWMutex
+	timeStep             time.Duration
+	allFailureLogTime    time.Time
+	allFailureCachedItem *I
+	itemMap              map[*I]*PoolNode[*I]
+	Master               *PoolNode[*I]
+	Slaves               []*PoolNode[*I]
 }
 
 type DurationTuple[I any] struct {
@@ -38,25 +45,31 @@ func (a DurationTupleList[I]) Len() int           { return len(a) }
 func (a DurationTupleList[I]) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a DurationTupleList[I]) Less(i, j int) bool { return a[i].Duration < a[j].Duration }
 
-func NewNode[I any](item I) *PoolNode[I] {
-	return &PoolNode[I]{
+func NewNode[I any](item *I) PoolNode[*I] {
+	return PoolNode[*I]{
 		Item: item,
 		Meta: PoolNodeMeta{
-			IsAlive:    true,
-			Reports:    []time.Time{},
-			LastReport: time.Time{},
-			BringAlive: time.Time{},
+			IsAlive:     true,
+			Reports:     0,
+			reportMutex: sync.Mutex{},
+			FirstReport: time.Time{},
+			LastReport:  time.Time{},
+			BringAlive:  time.Time{},
 		},
 	}
 }
 
 func NewEthClientMasterSlavePool(masterURL string,
-	slaveURLs []string) (*MasterSlavePool[*ethclient.Client], error) {
+	slaveURLs []string) (*MasterSlavePool[ethclient.Client], error) {
+	itemMap := make(map[*ethclient.Client]*PoolNode[*ethclient.Client], len(slaveURLs)+1)
+
 	// Setup master
 	ms, err := ethclient.Dial(masterURL)
 	if err != nil {
 		return nil, err
 	}
+	master := NewNode(ms)
+	itemMap[ms] = &master
 
 	// Setup slaves
 	slaves := []*PoolNode[*ethclient.Client]{}
@@ -65,25 +78,69 @@ func NewEthClientMasterSlavePool(masterURL string,
 		if err != nil {
 			return nil, err
 		}
-		slaves = append(slaves, NewNode(cl))
+		slave := NewNode(cl)
+		itemMap[cl] = &slave
+		slaves = append(slaves, &slave)
 	}
 
-	return &MasterSlavePool[*ethclient.Client]{
-		rwlock: sync.RWMutex{},
-		Master: NewNode(ms),
-		Slaves: slaves,
+	return &MasterSlavePool[ethclient.Client]{
+		rwlock:               sync.RWMutex{},
+		timeStep:             time.Duration(100 * time.Millisecond),
+		allFailureLogTime:    time.Time{},
+		allFailureCachedItem: nil,
+		itemMap:              itemMap,
+		Master:               &master,
+		Slaves:               slaves,
 	}, nil
 }
 
-func (m *MasterSlavePool[I]) Report(item I, timedOut bool) error {
+func (m *MasterSlavePool[I]) Report(item *I, timedOut bool) error {
 	if !timedOut {
 		return nil
 	}
-	// TODO
+	pool, ok := m.itemMap[item]
+	if !ok {
+		return errors.New("item not found")
+	}
+
+	now := time.Now()
+
+	// Short circuit in case of pool being not alive or last report too recently
+	if !pool.Meta.IsAlive || now.Sub(pool.Meta.LastReport) < m.timeStep {
+		return nil
+	}
+
+	pool.Meta.reportMutex.Lock()
+	defer pool.Meta.reportMutex.Unlock()
+
+	now = time.Now()
+	// Maybe somebody else reported while we were waiting for lock
+	// Short circuit in case of pool being not alive or last report too recently
+	if !pool.Meta.IsAlive || now.Sub(pool.Meta.LastReport) < m.timeStep {
+		return nil
+	}
+
+	pool.Meta.LastReport = now
+
+	// We forget all the failures accrued till now if counter addition
+	// start time has been since long.
+	if now.Sub(pool.Meta.FirstReport) > m.timeStep*20 {
+		pool.Meta.FirstReport = now
+		pool.Meta.Reports = 1
+	} else {
+		pool.Meta.Reports += 1
+	}
+
+	// If more than enough (40%) of timeSteps have resulted in failure, go to cooldown
+	if pool.Meta.Reports > 8 && now.Sub(pool.Meta.FirstReport) < m.timeStep*20 {
+		pool.Meta.IsAlive = false
+		pool.Meta.BringAlive = now.Add(m.timeStep * 10)
+	}
+
 	return nil
 }
 
-func (m *MasterSlavePool[I]) GetItem() I {
+func (m *MasterSlavePool[I]) GetItem() *I {
 	// Lock global RW lock for reads
 	m.rwlock.RLock()
 
@@ -99,7 +156,7 @@ func (m *MasterSlavePool[I]) GetItem() I {
 	if m.Master.Meta.BringAlive.Sub(now) == time.Duration(0) {
 		m.rwlock.RUnlock()
 		m.rwlock.Lock()
-		m.Master.Meta = NewReadyPoolNodeMeta()
+		MakeAlive(&m.Master.Meta)
 		m.rwlock.Unlock()
 		return m.Master.Item
 	}
@@ -107,7 +164,7 @@ func (m *MasterSlavePool[I]) GetItem() I {
 	// If master is not alive, nor is the time to bring it
 	// back online, check if any of the slaves is ready.
 	for _, slave := range m.Slaves {
-		sm := slave.Meta
+		sm := &slave.Meta
 		if sm.IsAlive {
 			m.rwlock.RUnlock()
 			return slave.Item
@@ -115,7 +172,7 @@ func (m *MasterSlavePool[I]) GetItem() I {
 		if sm.BringAlive.Sub(now) == time.Duration(0) {
 			m.rwlock.RUnlock()
 			m.rwlock.Lock()
-			slave.Meta = NewReadyPoolNodeMeta()
+			MakeAlive(&slave.Meta)
 			m.rwlock.Unlock()
 			return slave.Item
 		}
@@ -124,52 +181,70 @@ func (m *MasterSlavePool[I]) GetItem() I {
 	// If none of the others were successfully, we may have to
 	// wait till first rpc comes back online and send it
 	m.rwlock.RUnlock()
+
+	// Very expensive proposition, lot of mutex lock unlocks
 	return m.allFailureRecovery()
 }
 
-func (m *MasterSlavePool[I]) allFailureRecovery() I {
-	log.Warn("critical rpc failure. All upstreams in cooldown state. Blocking application")
+func (m *MasterSlavePool[I]) allFailureRecovery() *I {
 	currentTime := time.Now()
 
-	list := DurationTupleList[*PoolNode[I]]{}
+	m.rwlock.Lock()
+	defer m.rwlock.Unlock()
 
-	m.rwlock.RLock()
-	list = append(list, DurationTuple[*PoolNode[I]]{
+	// Critical section below
+	// The first thread that enters below does the hefty work of
+	// sort and wait, sets for a timeStep a cached item. Rest of
+	// the threads pick this item and return
+
+	if m.allFailureLogTime.Sub(currentTime) == time.Duration(0) {
+		// First thread doing hefty work
+		log.Warn("critical rpc failure. all upstreams in cooldown state. blocking application")
+	} else {
+		// If allFailureLogTime is in future, it can only be done by
+		// another thread which set this up recently. We can use the cached response hence.
+		return m.allFailureCachedItem
+	}
+
+	list := DurationTupleList[*PoolNode[*I]]{}
+
+	list = append(list, DurationTuple[*PoolNode[*I]]{
 		Duration: m.Master.Meta.BringAlive.Sub(currentTime),
 		Item:     m.Master,
 	})
 
 	for _, slave := range m.Slaves {
-		list = append(list, DurationTuple[*PoolNode[I]]{
+		list = append(list, DurationTuple[*PoolNode[*I]]{
 			Duration: slave.Meta.BringAlive.Sub(currentTime),
 			Item:     slave,
 		})
 	}
-	m.rwlock.RUnlock()
 
 	sort.Sort(list)
 
-	m.rwlock.Lock()
 	minDuration := list[0].Duration
 	time.Sleep(minDuration)
 
 	// Cleanup
 	for _, tuple := range list {
 		if tuple.Duration == minDuration {
-			tuple.Item.Meta = NewReadyPoolNodeMeta()
+			MakeAlive(&tuple.Item.Meta)
 		}
 	}
 
-	m.rwlock.Unlock()
+	m.allFailureLogTime = currentTime.Add(time.Duration(m.timeStep))
+	m.allFailureCachedItem = list[0].Item.Item
 
-	return list[0].Item.Item
+	return m.allFailureCachedItem
 }
 
-func NewReadyPoolNodeMeta() PoolNodeMeta {
-	return PoolNodeMeta{
-		IsAlive:    true,
-		Reports:    []time.Time{},
-		LastReport: time.Time{},
-		BringAlive: time.Time{},
-	}
+func MakeAlive(m *PoolNodeMeta) {
+	m.reportMutex.Lock()
+	defer m.reportMutex.Unlock()
+
+	m.IsAlive = true
+	m.Reports = 0
+	m.FirstReport = time.Time{}
+	m.LastReport = time.Time{}
+	m.BringAlive = time.Time{}
 }
